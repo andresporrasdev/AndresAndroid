cea0acee07fe8b18ab48e2ea76bcc3cb32ec692f
andresporrasdev <camilop1730@gmail.com>
2024-01-20 12:49:07 -0500

    Button Changing Text when press a button working

--- app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
@@ -3,7 +3,9 @@ package algonquin.cst2335.porr0016;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView myTextview = findViewById(R.id.textview);
+        TextView mytext = findViewById(R.id.textview);
         Button mybutton = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+
+
+
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String editString = myedit.getText().toString();
+                mytext.setText( "Your edit text has: " + editString);
+            }
+        });
+
     }
 }
