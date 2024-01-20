76b922311ad1baae0b1fb19268668c2fec33d8e7
andresporrasdev <camilop1730@gmail.com>
2024-01-20 09:15:15 -0500

    TextView and Button added and working

--- app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
@@ -3,6 +3,8 @@ package algonquin.cst2335.porr0016;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView myTextview = findViewById(R.id.textview);
+        Button mybutton = findViewById(R.id.mybutton);
     }
 }
