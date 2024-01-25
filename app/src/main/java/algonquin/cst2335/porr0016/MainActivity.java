b5fda7479b9c28dccdad5abd4ac2fdd78ce7c169
andresporrasdev <camilop1730@gmail.com>
2024-01-25 01:30:55 -0500

    Part 2 working totally

--- app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
@@ -2,29 +2,32 @@ package algonquin.cst2335.porr0016;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import algonquin.cst2335.porr0016.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView mytext = findViewById(R.id.textview);
-        Button mybutton = findViewById(R.id.mybutton);
-        EditText myedit = findViewById(R.id.myedittext);
 
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot()); //Loads XML on screen
 
+       // setContentView(R.layout.activity_main);
+        TextView myText = variableBinding.textview;
+        Button myButton = variableBinding.mybutton;
+        EditText myEdit = variableBinding.myedittext;
 
-        mybutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String editString = myedit.getText().toString();
-                mytext.setText( "Your edit text has: " + editString);
-            }
+        myButton.setOnClickListener(v -> {
+            String editString = myEdit.getText().toString();
+            myText.setText( "Your edit text has: " + editString);
         });
 
     }
