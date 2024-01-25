fbef2a91491c68a1eb89749dc85a7e3ea89040ec
andresporrasdev <camilop1730@gmail.com>
2024-01-25 14:44:06 -0500

    Part 4 ViewModel Pattern working

---
+++ app/src/main/java/algonquin/cst2335/porr0016/data/MainViewModel.java
@@ -0,0 +1,10 @@
+package algonquin.cst2335.porr0016.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+//    public String editString;
+    public MutableLiveData<String> editString = new MutableLiveData<>();
+}
---
+++ app/src/main/java/algonquin/cst2335/porr0016/ui2/MainActivity.java
@@ -0,0 +1,53 @@
+package algonquin.cst2335.porr0016.ui2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import algonquin.cst2335.porr0016.data.MainViewModel;
+import algonquin.cst2335.porr0016.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding variableBinding;
+    private MainViewModel model;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot()); //Loads XML on screen
+
+       // setContentView(R.layout.activity_main);
+        TextView myText = variableBinding.textview;
+//        Button myButton = variableBinding.mybutton;
+//        EditText myEdit = variableBinding.myedittext;
+//        String editString = myEdit.getText().toString();
+        model.editString.observe(this, s -> {
+            variableBinding.textview.setText("Your edit text has:" +s);
+        });
+
+//        variableBinding.textview.setText((CharSequence) model.editString);
+        variableBinding.mybutton.setOnClickListener(click -> {
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
+//            model.editString = variableBinding.myedittext.getText().toString();
+//            variableBinding.textview.setText("Your edit text has: " + model.editString);
+        });
+
+//        myButton.setOnClickListener(v -> myText.setText("Your edit text has: " + model.editString));
+
+//        myButton.setOnClickListener(v -> {
+//            String editString = myEdit.getText().toString();
+//            myText.setText( "Your edit text has: " + editString);
+//        });
+
+    }
+}
