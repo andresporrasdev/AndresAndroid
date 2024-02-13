e7ba5a605cc20367e7d08e8153aa075e072d2b40
Andres Porras <camilop1730@gmail.com>
2024-02-12 23:29:13 -0500

    Step1 Finished

--- app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
@@ -3,13 +3,48 @@ package algonquin.cst2335.porr0016;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String TAG = "MainActivity";
+
+
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        Log.d( TAG, "Message");
+
+
+
+
 
 
     }
