7221652f5494a51fadc49de3165ec41f15238d66
Andres Porras <camilop1730@gmail.com>
2024-02-13 19:38:27 -0500

    Part1 Working Webcamera taking photos and changing user profile picture

--- app/src/main/java/algonquin/cst2335/porr0016/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/porr0016/SecondActivity.java
@@ -1,16 +1,23 @@
 package algonquin.cst2335.porr0016;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     public void startActivity(Intent intent) {
         super.startActivity(intent);
@@ -29,6 +36,8 @@ public class SecondActivity extends AppCompatActivity {
         EditText phoneNumber = findViewById(R.id.editTextPhone);
         Button buttonCall = findViewById(R.id.buttonCall);
         TextView textView = findViewById(R.id.textView3);
+        ImageView profileImage = findViewById(R.id.profileImage);
+        Button buttonChangePicture = findViewById(R.id.buttonChangePicture);
 
         //Welcome message
         textView.setText("Welcome back "+emailAddress);
@@ -40,5 +49,31 @@ public class SecondActivity extends AppCompatActivity {
             call.setData(Uri.parse("tel:" + phoneNumberCall));
             startActivity(call);
         });
+
+
+//        buttonChangePicture.setOnClickListener(click ->{
+            //Taking a picture
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+                                Intent data = result.getData();
+                                Bitmap thumbnail = null;
+                                if (data != null) {
+                                    thumbnail = data.getParcelableExtra("data");
+                                }
+                                profileImage.setImageBitmap(thumbnail);
+
+
+                            }
+                        }
+                    });
+            cameraResult.launch(cameraIntent);
+
+//        });
     }
 }
