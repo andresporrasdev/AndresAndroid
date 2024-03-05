63062483202a832296c39fa19df29bd62ef0d1b2
Andres Porras <camilop1730@gmail.com>
2024-03-05 10:33:26 -0500

    Lab05 CheckPssword function working

--- app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/porr0016/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.porr0016;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Variable declaration
+        TextView tv = findViewById(R.id.textView);
+        EditText et = findViewById(R.id.editTextTextPassword);
+        Button btn = findViewById(R.id.button);
 
+        btn.setOnClickListener(clk ->{
+            String password = et.getText().toString();
+            checkPasswordComplexity(password);
+        });
+    }
+
+    /**
+     * This function should check if this string has an Upper Case letter,
+     * a lower case letter, a number, and a special symbol.
+     *
+     * @param pw The String object that we are checking
+     * @return Returns true if the String meets the requirements
+     */
+    private boolean checkPasswordComplexity(String pw) {
+        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+
+        String specialCharacters = "#$%^&*!@?";
+
+        for (int i = 0; i < pw.length(); i++) {
+            char c = pw.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                foundUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                foundLowerCase = true;
+            } else if (Character.isDigit(c)) {
+                foundNumber = true;
+            } else if (isSpecialCharacter(c)) {
+                foundSpecial = true;
+            }
+        }
+
+        if (!foundUpperCase) {
+            Toast.makeText(MainActivity.this, "You are missing an upper case letter.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundLowerCase) {
+            Toast.makeText(MainActivity.this, "You are missing a lower case letter.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundNumber) {
+            Toast.makeText(MainActivity.this, "You are missing a number.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundSpecial) {
+            Toast.makeText(MainActivity.this, "You are missing a special character (#$%^&*!@?).", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     *
+     * This function should check if this character is a special symbol.
+     *
+     * @param c The character we are checking
+     * @return true if c is special symbol
+     */
+    boolean isSpecialCharacter(char c) {
+        switch (c) {
+            case '#':
+            case '$':
+            case '%':
+            case '^':
+            case '&':
+            case '*':
+            case '!':
+            case '@':
+            case '?':
+                return true;
+            default:
+                return false;
+        }
     }
 }
