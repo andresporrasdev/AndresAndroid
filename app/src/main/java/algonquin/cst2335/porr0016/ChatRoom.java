956149994452ad141cf2bec7fa143f72e04b423a
Andres Porras <camilop1730@gmail.com>
2024-03-12 00:03:58 -0400

    No errors but not working

--- app/src/main/java/algonquin/cst2335/porr0016/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/porr0016/ChatRoom.java
@@ -3,6 +3,7 @@ package algonquin.cst2335.porr0016;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -10,40 +11,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 import algonquin.cst2335.porr0016.databinding.ActivityChatRoomBinding;
+import algonquin.cst2335.porr0016.databinding.SentMessageBinding;
 
 public class ChatRoom extends AppCompatActivity {
     ActivityChatRoomBinding binding;
 
+    ArrayList<String> messages = new ArrayList<>();
+    private RecyclerView.Adapter myAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.sendButton.setOnClickListener(click -> {
+            messages.add(binding.textInput.getText().toString());
+            myAdapter.notifyItemInserted(messages.size() - 1);
+            //clear the previous text:
+            binding.textInput.setText("");
+        });
+
         class MyRowHolder extends RecyclerView.ViewHolder {
+            TextView messageText;
+            TextView timeText;
+
             public MyRowHolder(@NonNull View itemView) {
                 super(itemView);
+                messageText = itemView.findViewById(R.id.message);
+                timeText = itemView.findViewById(R.id.time);
             }
         }
-        binding.recycleView.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
+        binding.recycleView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+                SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
+                return new MyRowHolder(binding.getRoot());
             }
 
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-
-            }
+                holder.messageText.setText("");
+                holder.timeText.setText("");
+                String obj = messages.get(position);
+                holder.messageText.setText(obj);
+            };
 
             @Override
             public int getItemCount() {
+                return messages.size();
+            };
+
+            @Override
+            public int getItemViewType(int position) {
                 return 0;
-            }
+            };
         });
+
+        binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
+
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat_room);
